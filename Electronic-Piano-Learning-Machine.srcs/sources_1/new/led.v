29451292de00f8f84a792c3eaa12e94a41efdd26
Zaoerdd <zaoerdd@gmail.com>
2023-12-17 00:02:31 +0800

    Update led.v

--- Electronic-Piano-Learning-Machine.srcs/sources_1/new/led.v
+++ Electronic-Piano-Learning-Machine.srcs/sources_1/new/led.v
@@ -3,3 +3,20 @@ module led(big_dip_switches_in, led_out);
     output [7:0] led_out;
     assign led_out = big_dip_switches_in;
 endmodule
+
+// set_property PACKAGE_PIN F6 [get_ports {led_out[7]}]
+// set_property PACKAGE_PIN G4 [get_ports {led_out[6]}]
+// set_property PACKAGE_PIN G3 [get_ports {led_out[5]}]
+// set_property PACKAGE_PIN J4 [get_ports {led_out[4]}]
+// set_property PACKAGE_PIN H4 [get_ports {led_out[3]}]
+// set_property PACKAGE_PIN J3 [get_ports {led_out[2]}]
+// set_property PACKAGE_PIN J2 [get_ports {led_out[1]}]
+// set_property PACKAGE_PIN K2 [get_ports {led_out[0]}]
+// set_property IOSTANDARD LVCMOS33 [get_ports {led_out[7]}]
+// set_property IOSTANDARD LVCMOS33 [get_ports {led_out[6]}]
+// set_property IOSTANDARD LVCMOS33 [get_ports {led_out[5]}]
+// set_property IOSTANDARD LVCMOS33 [get_ports {led_out[4]}]
+// set_property IOSTANDARD LVCMOS33 [get_ports {led_out[3]}]
+// set_property IOSTANDARD LVCMOS33 [get_ports {led_out[2]}]
+// set_property IOSTANDARD LVCMOS33 [get_ports {led_out[1]}]
+// set_property IOSTANDARD LVCMOS33 [get_ports {led_out[0]}]
