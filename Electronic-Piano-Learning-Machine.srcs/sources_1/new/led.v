f394e1910e483f7d49e8c1ae2bb9146397f57202
Zaoerdd <zaoerdd@gmail.com>
2023-12-16 23:02:05 +0800

    led

--- Electronic-Piano-Learning-Machine.srcs/sources_1/new/led.v
+++ Electronic-Piano-Learning-Machine.srcs/sources_1/new/led.v
@@ -1,5 +1,5 @@
 module led(big_dip_switches_in, led_out);
-input [7:0] big_dip_switches_in;
-output [7:0] led_out;
-assign led_out = big_dip_switches_in;
+    input [7:0] big_dip_switches_in;
+    output [7:0] led_out;
+    assign led_out = big_dip_switches_in;
 endmodule
